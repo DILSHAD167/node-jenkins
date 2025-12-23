a551f3e5333399292349112d8641f09963ff64f8
DILSHAD167 <dilshadaws167@gmail.com>
2025-12-23 17:16:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Jenkins CI/CD 🚀");
+});
+
+app.listen(3000, () => console.log("Running on 3000"));
