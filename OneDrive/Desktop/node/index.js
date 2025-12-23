e178d154ef3e5faf88e42cea2291dd9f3b724d8c
DILSHAD167 <dilshadaws167@gmail.com>
2025-12-23 17:16:41 +0530

    Delete OneDrive/Desktop/node directory

--- OneDrive/Desktop/node/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from Jenkins CI/CD 🚀");
-});
-
-app.listen(3000, () => console.log("Running on 3000"));
